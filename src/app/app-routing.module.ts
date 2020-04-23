966676e74a2ee2ec21f7dcb03427f39b26474f1d
Shakikhanli <shekikhanli@gmail.com>
2020-04-23 09:09:44 +0200

    Adding basic desing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { MusicComponent } from './music/music.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'pictures', component: PicturesComponent },
+  { path: 'music', component: MusicComponent },
   { path: 'about', component: AboutComponent }
 ];
 
